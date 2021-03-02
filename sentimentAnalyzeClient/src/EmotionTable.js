ea173c7d13fbd25d4a33edd2b0d5663c53e93097
your_github_username <youremail@domain.com>
2021-03-02 23:38:59 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
+    renderEmotions(){
+        const { emotions = {} } = this.props;
+        return Object.keys(emotions).map((key, i) => {
+            return (
+            <React.Fragment>
+                <tr key={i}>
+                    <td>{key}</td>
+                    <td>{emotions[key]}</td>
+                </tr>
+            </React.Fragment>);
+        });
+
+    }
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
             {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+                this.renderEmotions()
             }
             </tbody>
           </table>
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,87 @@
 const express = require('express');
+const dotenv = require ('dotenv');
+dotenv.config();
 const app = new express();
 
 app.use(express.static('client'))
-
 const cors_app = require('cors');
 app.use(cors_app());
 
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+            version: '2020-08-01',
+            authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+const analyzeParams = {
+    'url': 'www.ibm.com',
+    'features': {
+        'entities': {
+        'emotion': true,
+        'sentiment': true,
+        'limit': 2,
+        },
+    },
+};
+
 app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
+    res.render('index.html'); 
+});
 
 app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+   let naturalLanguageUnderstanding = getNLUInstance();
+   naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+        return res.send(analysisResults.result.entities[0].emotion);
+        })
+    .catch(err => {
+            console.log('error:', err);
+        });  
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
-});
+    let naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            return res.send(analysisResults.result.entities[0].sentiment.label);
+            })
+        .catch(err => {
+                console.log('error:', err);
+            });  
+}); 
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+   let naturalLanguageUnderstanding = getNLUInstance();
+   naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+        return res.send(analysisResults.result.entities[0].emotion);
+        })
+    .catch(err => {
+            console.log('error:', err);
+        });  
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            return res.send(analysisResults.result.entities[0].sentiment.label);
+            })
+        .catch(err => {
+                console.log('error:', err);
+            });  
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
